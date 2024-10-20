28e22eea9c5891906f77f3bc92353c13f8ddad41
Hunter MacNab <hunter.amac@gmail.com>
2024-10-20 12:09:54 -0700

    get tests to pass when actually reading files

--- TestMdToUE5StringTable/test.cpp
+++ TestMdToUE5StringTable/test.cpp
@@ -3,22 +3,26 @@
 #include <filesystem>
 #include "../MdToUE5StringTables/MdToCSV.h"
 
+
+
 TEST(MdToCSV, HighLevelSimple1) {
 	// ARRANGE - Setup filepath to Portrayal File
-	std::filesystem::path PortrayalFile = "D:\\dev\\TestMdToUE5StringTable\\TestFilesDirectory\\p_text1.md";
-
+	// 
+	// Used to determine where test code was running
+	//std::string curPath = std::filesystem::current_path().string();
+	std::filesystem::path PortrayalFile = "../../../TestMdToUE5StringTable/TestFilesDirectory/p_text1.md";
+	
 	// ACT - Read premade p_text1.md from testing directory (top level)
 
 	std::string result = MdPortrayalFileToCSV(PortrayalFile);
 	// ASSERT - Result matches expected write to csv file
-
 	std::string ExpectedCsvParse = "\"p_text1\",\"It was a dark and stormy night.\"";
 	EXPECT_EQ(ExpectedCsvParse, result);
 }
 
 TEST(MdToCSV, HighLevelSimple2) {
 	// ARRANGE - Setup filepath to Portrayal File
-	std::filesystem::path PortrayalFile = "D:\\dev\\TestMdToUE5StringTable\\TestFilesDirectory\\p_text2.md";
+	std::filesystem::path PortrayalFile = "../../../TestMdToUE5StringTable/TestFilesDirectory/p_text2.md";
 
 	// ACT - Read premade p_text1.md from testing directory (top level)
 
